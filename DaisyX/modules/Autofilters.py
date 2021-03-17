ab920b56fd8999cd03ee519744d85c7f27fc73b0
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-17 17:02:25 +0530

    Update Autofilters.py

--- DaisyX/modules/Autofilters.py
+++ DaisyX/modules/Autofilters.py
@@ -16,7 +16,7 @@ from pyrogram.types import (
     CallbackQuery
 )
 
-from DaisyX.services.pyrogramuserbot import pubot as Bot
+from DaisyX.services.pyrogram import pbot as Bot
 from DaisyX.db.autofilter_db import searchquery
 from DaisyX.modules.autofilters_channel import deleteallfilters
 #from config import AUTH_USERS
